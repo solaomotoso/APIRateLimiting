3ee18c28f4886da6fb27d392b05b8ce1eca4a391
olusola omotoso <solaomotoso2002@yahoo.com>
2023-12-03 09:40:32 +0100

    RateLimitTest

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using RateLimit;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddPolicy("CustomRateLimiting", new CustomRateLimiting(true,3,0,1));
+});
+
+var app = builder.Build();
+app.UseRateLimiter();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
